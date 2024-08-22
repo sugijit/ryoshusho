25641b5edbd54835ec5a54c1861e62f97ea5aa02
sugijit <b.suldsaikhan@gmail.com>
2024-08-22 21:04:29 +0900

    little fix

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,6 @@ use App\Http\Controllers\ReceiptController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
